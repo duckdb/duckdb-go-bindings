2981e8dc4dba1e5d987963527744b1c6b95b3b34
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-03-19 12:48:23 +0100

    try open sqlite

--- windows-amd64/bindings_test.go
+++ windows-amd64/bindings_test.go
@@ -1,6 +1,8 @@
 package duckdb_go_bindings
 
 import (
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,4 +10,67 @@ import (
 
 func TestOpen(t *testing.T) {
 	require.Equal(t, IdxT(2048), VectorSize())
+}
+
+func TestOpenDB(t *testing.T) {
+	var db Database
+
+	dsn := "sqlite:pets.sqlite"
+	parsedDSN, err := url.Parse(dsn)
+	require.NoError(t, err)
+
+	config, err := prepareConfig(parsedDSN)
+	require.NoError(t, err)
+	defer DestroyConfig(&config)
+
+	connStr := getConnString(dsn)
+	var errMsg string
+	if OpenExt(connStr, &db, config, &errMsg) == StateError {
+		Close(&db)
+		panic("error")
+	}
+}
+
+func getConnString(dsn string) string {
+	idx := strings.Index(dsn, "?")
+	if idx < 0 {
+		idx = len(dsn)
+	}
+	return dsn[0:idx]
+}
+
+func prepareConfig(parsedDSN *url.URL) (Config, error) {
+	var config Config
+	if CreateConfig(&config) == StateError {
+		DestroyConfig(&config)
+		panic("error")
+	}
+
+	if err := setConfigOption(config, "duckdb_api", "go"); err != nil {
+		return config, err
+	}
+
+	// Early-out, if the DSN does not contain configuration options.
+	if len(parsedDSN.RawQuery) == 0 {
+		return config, nil
+	}
+
+	for k, v := range parsedDSN.Query() {
+		if len(v) == 0 {
+			continue
+		}
+		if err := setConfigOption(config, k, v[0]); err != nil {
+			return config, err
+		}
+	}
+
+	return config, nil
+}
+
+func setConfigOption(config Config, name string, option string) error {
+	if SetConfig(config, name, option) == StateError {
+		DestroyConfig(&config)
+		panic("error")
+	}
+	return nil
 }
