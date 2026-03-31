2698d87dde3be41237dba6c57627c2c6247eae36
dentinyhao <dentinyhao@gmail.com>
2026-03-31 04:23:34 -0700

    Fix alloc count

--- bindings.go
+++ bindings.go
@@ -3302,7 +3302,7 @@ func TableFunctionGetClientContext(info BindInfo, outCtx *ClientContext) {
 	C.duckdb_table_function_get_client_context(info.data(), &ctx)
 	outCtx.Ptr = unsafe.Pointer(ctx)
 	if debugMode {
-		decrAllocCount("ctx")
+		incrAllocCount("ctx")
 	}
 }
 
