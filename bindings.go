2ddacec9d78d7ff933a6658ef98886f6476f3fef
dentinyhao <dentinyhao@gmail.com>
2026-03-31 04:32:32 -0700

    prevent useless incr

--- bindings.go
+++ bindings.go
@@ -3804,7 +3804,7 @@ func StreamFetchChunk(res Result, outChunk *DataChunk) State {
 
 func FetchChunk(res Result) DataChunk {
 	chunk := C.duckdb_fetch_chunk(res.data)
-	if debugMode {
+	if debugMode && chunk != nil {
 		incrAllocCount("chunk")
 	}
 	return DataChunk{
